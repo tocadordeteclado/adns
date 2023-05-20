8641da9afed5dc442048fec17e1334805709d622
Carlos Eduardo <134072333+tocadordeteclado@users.noreply.github.com>
2023-05-20 11:12:48 -0300

    Atualizar o setup.c

--- repo/adns/src/setup.c
+++ repo/adns/src/setup.c
@@ -4,6 +4,7 @@
  *     - gestão do estado global.
  *
  * Direito Autoral (C) {{ ano(); }}  {{ nome_do_autor(); }}
+ * Direito Autoral (C) 2023 Messias Izidoro
  *
  * Este programa é um software livre: você pode redistribuí-lo
  * e/ou modificá-lo sob os termos da Licença Pública do Cavalo

d6e5226c738a80c6b472ca23b75075e2167e1e0a
Carlos Eduardo <134072333+tocadordeteclado@users.noreply.github.com>
2023-05-20 11:05:34 -0300

    Atualizar o adns.h

--- repo/adns/src/adns.h
+++ repo/adns/src/adns.h
@@ -2,6 +2,7 @@
  * adns.h - API adns visível ao usuário.
  *
  * Direito Autoral (C) {{ ano(); }}  {{ nome_do_autor(); }}
+ * Direito Autoral (C) 2023 Messias Izidoro
  *
  * Este programa é um software livre: você pode redistribuí-lo
  * e/ou modificá-lo sob os termos da Licença Pública do Cavalo

d72ba82f5631a17d8240d1f87ce7a1ea538ce8bd
Carlos Eduardo <134072333+tocadordeteclado@users.noreply.github.com>
2023-05-20 10:55:08 -0300

    Atualizar o addrtext.c

--- repo/adns/client/addrtext.c
+++ repo/adns/client/addrtext.c
@@ -3,6 +3,7 @@
  *   não faz parte da biblioteca
  *
  * Direito Autoral (C) {{ ano(); }}  {{ nome_do_autor(); }}
+ * Direito Autoral (C) 2023 Messias Izidoro
  *
  * Este programa é um software livre: você pode redistribuí-lo
  * e/ou modificá-lo sob os termos da Licença Pública do Cavalo

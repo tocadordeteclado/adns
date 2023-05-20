dd45da675b62a6d8648e7bc06904c34c5b7c9298
Carlos Eduardo <134072333+tocadordeteclado@users.noreply.github.com>
2023-05-20 11:11:00 -0300

    Atualizar o poll.c

--- repo/adns/src/poll.c
+++ repo/adns/src/poll.c
@@ -2,6 +2,7 @@
  * poll.c - wrappers para poll(2).
  *
  * Direito Autoral (C) {{ ano(); }}  {{ nome_do_autor(); }}
+ * Direito Autoral (C) 2023 Messias Izidoro
  *
  * Este programa é um software livre: você pode redistribuí-lo
  * e/ou modificá-lo sob os termos da Licença Pública do Cavalo
